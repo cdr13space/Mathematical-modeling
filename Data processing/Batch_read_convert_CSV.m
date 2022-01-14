584433e4f93381ec3c706cf320549ae3d39c15d3
Warrick Xu <xwj770427414@126.com>
2022-01-14 15:28:47 +0800

    Change Dataset and some Methods

--- Data processing/Batch_read_convert_CSV.m
+++ Data processing/Batch_read_convert_CSV.m
@@ -1,6 +1,9 @@
 xdir= dir(fullfile("Data\initial Data",'*.csv'))
 for k = 1:20
     file = xlsread([xdir(k).folder,'\',xdir(k).name])
-    result = fillmissing(file,'pchip')
+    % pchip 分段三次 Hermite 插值多项式
+    % result = fillmissing(file,'pchip')  
+    % spline 三次方样条数据插值
+    result = fillmissing(file,'spline')
     csvwrite(['Data\Preprocess Data\',xdir(k).name],result)
 end
