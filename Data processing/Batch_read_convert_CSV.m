ac08d23a0c528fead79378762faa7369e036d13f
Warrick Xu <xwj770427414@126.com>
2022-01-14 14:17:17 +0800

    Add simulation work

--- Data processing/Batch_read_convert_CSV.m
+++ Data processing/Batch_read_convert_CSV.m
@@ -1,6 +1,6 @@
 xdir= dir(fullfile("Data\initial Data",'*.csv'))
 for k = 1:20
-    file = xlsread([xdir(k).folder,'\',xdir(1).name])
+    file = xlsread([xdir(k).folder,'\',xdir(k).name])
     result = fillmissing(file,'pchip')
     csvwrite(['Data\Preprocess Data\',xdir(k).name],result)
 end
