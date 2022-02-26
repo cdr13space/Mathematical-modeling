aee2adbca21c125e993b377a6d60a8ea9316182d
Warrick Xu <xwj770427414@126.com>
2022-02-26 15:10:44 +0800

    remove

--- Simulation practice/2018 E/Batch_read_convert_CSV.m
+++
@@ -1,9 +0,0 @@
-xdir = dir(fullfile("E:\Git Code\Mathematical-modeling\Simulation practice\2018 E\Preprocess Data\Break up",'*.csv'))
-for k = 1:20
-    file = xlsread([xdir(k).folder,'\',xdir(k).name])
-    % pchip 分段三次 Hermite 插值多项式
-    result = fillmissing(file,'pchip')  
-    % spline 三次方样条数据插值
-    % result = fillmissing(file,'spline')
-    csvwrite(['E:\Git Code\Mathematical-modeling\Simulation practice\2018 E\Preprocess Data\interpolation process\',xdir(k).name],result)
-end
